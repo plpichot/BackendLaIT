00358a6c2ec276e4eee69a7969cad1331dc16947
Pierre-Louis Pichot <plpichot@MacBook-Air-de-Pierre-Louis.local>
2023-08-03 10:45:21 +0200

    new cors definition

--- src/main.js
+++ src/main.js
@@ -4,8 +4,12 @@ const { AppModule } = require('./app.module');
 
 async function bootstrap() {
 
-  const app = await NestFactory.create(AppModule, { cors: true });
-
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+  });
   app.use(express.json({ limit: '10mb' }));
   await app.listen(3000)
 }
