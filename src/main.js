5d2362f7d702fcc7f42c30c5871b8f6f9caad8b4
Pierre-Louis Pichot <plpichot@MacBook-Air-de-Pierre-Louis.local>
2023-08-03 10:28:43 +0200

    without htpps commentaire

--- src/main.js
+++ src/main.js
@@ -1,19 +1,9 @@
 const { NestFactory } = require('@nestjs/core') ;
 const express = require('express');
 const { AppModule } = require('./app.module');
-/*import * as fs from 'fs';
-import * as https from 'https'; */
 
 async function bootstrap() {
 
-  /*const httpsOptions = {
-    key: fs.readFileSync('/Users/plpichot/Desktop/backend/certificates/key.pem', 'utf8'),
-    cert: fs.readFileSync('/Users/plpichot/Desktop/backend/certificates/cert.pem', 'utf8'),
-  };
-
-  const app = await NestFactory.create(AppModule, {
-    httpsOptions,
-  });*/
   const app = await NestFactory.create(AppModule, { cors: true });
 
   app.use(express.json({ limit: '10mb' }));
